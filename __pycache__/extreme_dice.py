5bf51f2c70815ad7c218386a5fb551d252e47f0a
David Karim Jr <davidkarimjr@gmail.com>
2022-04-07 00:42:00 +0000

    extreme_dice

---
+++ __pycache__/extreme_dice.py
@@ -0,0 +1,13 @@
+#3_2_double_dice
+import random
+for x in range (1,11):
+    throw_1 = random.randint(1, 6)
+    throw_2 = random.randint(1, 6)
+    total = throw_1 + throw_2
+    print(total)
+    if total == 7:
+        print('seven Thrown!')
+if total == 11:
+    print('Eleven Thrown!')
+if throw_1 == throw_2:
+ print('Double Thrown!')
